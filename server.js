0514df58d19cac57194e8f10aee1b02149c34931
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-06-20 20:45:02 +0300

    add env

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const mongoose = require("mongoose");
+ const mongoose = require("mongoose");
 
-const app = require("./app");
-const DB_HOST="mongodb+srv://Nazzik92:Nazzik92@cluster1.yoszk3g.mongodb.net/contact-list?retryWrites=true&w=majority";
-
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    console.log("Database connection successful");
-    app.listen(8000);
-    
-  })
-  .catch((error) => {
-    console.log(error);
-    process.exit(1);
-  });
+  const app = require("./app");
+  
+  const { DB_HOST, PORT = 8000 } = process.env;
+  
+  mongoose.set("strictQuery", true);
+  
+  mongoose
+    .connect(DB_HOST)
+    .then(() => {
+      console.log("Database connection successful");
+  
+      app.listen(PORT, () => {
+        console.log("Server running. Use our API on port: 8000");
+      });
+    })
+    .catch((error) => {
+      console.log(error.message);
+      process.exit(1);
+    });
