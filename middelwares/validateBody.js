29cea412258219894d5b5a54f8c7ba7d09c4a13b
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-06-24 15:23:14 +0300

    corectionValidateBody

--- middelwares/validateBody.js
+++ middelwares/validateBody.js
@@ -1,11 +1,19 @@
 const { HttpError } = require("../utils");
 
-const validateBody = (schemas) => {
+const validateBody = schema => {
   const fn = (req, res, next) => {
-    const { error } = schemas.validate(req.body);
-    if (error) {
-      res.status(400).json({ message: "missing field favorite" });
+    console.log(req.method);
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+      if (req.method === 'PATCH') {
+        res.status(400).json({ message: 'missing field favorite'});
+      }
+      res.status(400);
     }
+    const { error } = schema.validate(req.body);
+    if (error) {
+      res.status(400).json(error.message);
+    }
+
     next(error);
   };
   return fn;
