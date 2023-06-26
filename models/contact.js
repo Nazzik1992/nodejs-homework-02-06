0b205f2cf21628cc7ad4a6101027e9c1d33016e6
Nazzik1992 <tkachuknazarii15@gmail.com>
2023-06-26 21:51:56 +0300

    corectSchema

--- models/contact.js
+++ models/contact.js
@@ -26,16 +26,16 @@ contactSchema.post("save", handleMongooseError);
 
 const contactAddSchema = Joi.object({
   name: Joi.string().required().messages({
-    "any.required": `"name" is a required field`,
-    "string.empty": `"name" cannot be an empty field`,
+    "any.required": `missing required 'name' field`,
+    "string.empty": `'name' cannot be an empty field`,
   }),
   email: Joi.string().required().messages({
-    "any.required": `"email" is a required field`,
+    "any.required": `missing required 'email' field`,
     "string.empty": `"email" cannot be an empty field`,
   }),
   phone: Joi.string().required().messages({
-    "any.required": `"phone" is a required field`,
-    "string.empty": `"phone" cannot be an empty field`,
+    "any.required": `missing required 'phone' field`,
+    "string.empty": `'email' cannot be an empty field`,
   }),
   favorite: Joi.boolean()
 });
